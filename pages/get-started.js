3bbe8ec9f681738af5c120353b2d885e8b7b570b
Sumitmaithani <sumitmaithani824@gmail.com>
2024-08-10 10:39:17 +0530

    added get started page

--- pages/get-started.js
+++ pages/get-started.js
@@ -1,19 +1,19 @@
-import Head from "next/head";
-import Input from "../components/ui/Input";
-import Button from "../components/ui/Button";
-import Checkbox from "../components/ui/Checkbox";
+import { useState } from "react";
 import Navbar from "../components/ui/Navbar";
 import Image from "next/image";
 import spurgear from "../public/images/Spur_Gear.svg";
 import NavLink from "../components/ui/NavLink";
 
 export default function GetStarted() {
-  const servicesItems = [
-    "Mobile development",
-    "UI/UX Design",
-    "web development",
-    "SEO",
-  ];
+  const [selectedGear, setSelectedGear] = useState(null);
+
+  const handleGearSelect = (gear) => {
+    setSelectedGear(gear);
+  };
+
+  const handleNextClick = () => {
+    window.location.href = `/gear-info?gear=${selectedGear}`;
+  };
 
   return (
     <>
@@ -25,15 +25,42 @@ export default function GetStarted() {
           </h1>
 
           <div className="flex mt-12 justify-between w-full gap-20">
-            <div className="flex flex-col gap-3 text-center border-solid	border-2 border-grey-500 rounded-2xl py-6 px-5 flex-1">
+            <div
+              className={`flex flex-col gap-3 text-center border-solid border-2 rounded-2xl py-6 px-5 flex-1 ${
+                selectedGear === "Spur Gear"
+                  ? "border-blue-600"
+                  : "border-grey-500"
+              } hover:border-grey-600 ${
+                selectedGear !== "Spur Gear" ? "hover:opacity-50" : ""
+              } hover:cursor-pointer`}
+              onClick={() => handleGearSelect("Spur Gear")}
+            >
               <Image src={spurgear} alt="gear" width={200} />
               <h4>Spur Gear</h4>
             </div>
-            <div className="flex flex-col gap-3  text-center border-solid	border-2 border-grey-500 rounded-2xl py-6 px-5 flex-1">
+            <div
+              className={`flex flex-col gap-3 text-center border-solid border-2 rounded-2xl py-6 px-5 flex-1 ${
+                selectedGear === "Helical Gear"
+                  ? "border-blue-600"
+                  : "border-grey-500"
+              } hover:border-grey-600 ${
+                selectedGear !== "Helical Gear" ? "hover:opacity-50" : ""
+              } hover:cursor-pointer`}
+              onClick={() => handleGearSelect("Helical Gear")}
+            >
               <Image src={spurgear} alt="gear" width={200} />
               <h4>Helical Gear</h4>
             </div>
-            <div className="flex flex-col gap-3  text-center border-solid	border-2 border-grey-500 rounded-2xl py-6 px-5 flex-1">
+            <div
+              className={`flex flex-col gap-3 text-center border-solid border-2 rounded-2xl py-6 px-5 flex-1 ${
+                selectedGear === "Double Helical Gear"
+                  ? "border-blue-600"
+                  : "border-grey-500"
+              } hover:border-grey-600 ${
+                selectedGear !== "Double Helical Gear" ? "hover:opacity-50" : ""
+              } hover:cursor-pointer`}
+              onClick={() => handleGearSelect("Double Helical Gear")}
+            >
               <Image src={spurgear} alt="gear" width={200} />
               <h4>Double Helical Gear</h4>
             </div>
@@ -50,7 +77,14 @@ export default function GetStarted() {
 
             <NavLink
               href="/get-started"
-              className="text-white bg-blue-600 hover:bg-blue-500 active:bg-gray-900 "
+              className={`text-white ${
+                selectedGear
+                  ? "bg-blue-600 hover:bg-blue-500 active:bg-gray-900"
+                  : "bg-gray-400 cursor-not-allowed"
+              }`}
+              onClick={
+                selectedGear ? handleNextClick : (e) => e.preventDefault()
+              }
             >
               Next
             </NavLink>
