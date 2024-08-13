c02d723d4cd20263b0bc8ac53774231c77ec65e1
Sumitmaithani <sumitmaithani824@gmail.com>
2024-08-13 18:49:01 +0530

    fix: loading issue

--- pages/gear-info/index.jsx
+++ pages/gear-info/index.jsx
@@ -2,136 +2,117 @@
 import Input from "../../components/ui/Input";
 import Button from "../../components/ui/Button";
 import Navbar from "../../components/ui/Navbar";
-import animation from "../../public/animation.json";
 import { useState } from "react";
 import { useRouter } from "next/router";
 
-import Lottie from "lottie-react";
-
 export default function Index() {
-  const [showLoading, setShowLoading] = useState(false);
   const [data, setData] = useState({});
   const router = useRouter();
   const { gear } = router.query;
 
   const handleCalculation = (e) => {
     e.preventDefault();
-    setShowLoading(true);
 
-    setTimeout(() => {
-      setShowLoading(false);
-      const queryString = new URLSearchParams(data).toString();
-      router.push(`/result?${queryString}`);
-    }, 3000);
+    const queryString = new URLSearchParams(data).toString();
+    router.push(`/result?${queryString}`);
   };
 
   return (
     <>
       <Navbar />
 
-      {showLoading ? (
-        <Lottie
-          animationData={animation}
-          loop
-          className="pt-8 pb-12 pl-96 pr-96"
-        />
-      ) : (
-        <>
-          <h1 className="text-gray-800 text-3xl font-semibold sm:text-4xl pl-72 pr-72 pt-4">
-            Gear properties
-          </h1>
-          <div className="pt-8 pb-12 pl-96 pr-96">
-            <form
-              onSubmit={handleCalculation}
-              className="space-y-5 font-medium"
-            >
+      <>
+        <h1 className="text-gray-800 text-3xl font-semibold sm:text-4xl pl-72 pr-72 pt-4">
+          Gear properties
+        </h1>
+        <div className="pt-8 pb-12 pl-96 pr-96">
+          <form onSubmit={handleCalculation} className="space-y-5 font-medium">
+            <div>
+              <label>Number of teeth* (N):</label>
+              <Input
+                aria-label="Number of teeth"
+                type="text"
+                required
+                className="mt-2 focus:border-blue-600"
+                onChange={(e) => setData({ ...data, teeth: e.target.value })}
+              />
+            </div>
+            <div>
+              <label>Pitch diameter* (D):</label>
+              <Input
+                aria-label="Pitch diameter"
+                type="text"
+                required
+                className="mt-2 focus:border-blue-600"
+                onChange={(e) =>
+                  setData({ ...data, pitchDiameter: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <label>Diametral pitch* (P):</label>
+              <Input
+                aria-label="Diametral pitch"
+                type="text"
+                required
+                className="mt-2 focus:border-blue-600"
+                onChange={(e) =>
+                  setData({ ...data, diametralPitch: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <label>Pressure Angle* (PA):</label>
+              <Input
+                aria-label="Pressure Angle"
+                type="text"
+                required
+                className="mt-2 focus:border-blue-600"
+                onChange={(e) =>
+                  setData({ ...data, pressureAngle: e.target.value })
+                }
+              />
+            </div>
+            {gear === "Spur Gear" ? (
               <div>
-                <label>Number of teeth* (N):</label>
+                <label>Sut* :</label>
                 <Input
-                  aria-label="Number of teeth"
+                  aria-label="Sut"
                   type="text"
                   required
                   className="mt-2 focus:border-blue-600"
-                  onChange={(e) => setData({ ...data, teeth: e.target.value })}
+                  onChange={(e) => setData({ ...data, sut: e.target.value })}
                 />
               </div>
-              <div>
-                <label>Pitch diameter* (D):</label>
-                <Input
-                  aria-label="Pitch diameter"
-                  type="text"
-                  required
-                  className="mt-2 focus:border-blue-600"
-                  onChange={(e) =>
-                    setData({ ...data, pitchDiameter: e.target.value })
-                  }
-                />
-              </div>
-              <div>
-                <label>Diametral pitch* (P):</label>
-                <Input
-                  aria-label="Diametral pitch"
-                  type="text"
-                  required
-                  className="mt-2 focus:border-blue-600"
-                  onChange={(e) =>
-                    setData({ ...data, diametralPitch: e.target.value })
-                  }
-                />
-              </div>
-              <div>
-                <label>Pressure Angle* (PA):</label>
-                <Input
-                  aria-label="Pressure Angle"
-                  type="text"
-                  required
-                  className="mt-2 focus:border-blue-600"
-                  onChange={(e) =>
-                    setData({ ...data, pressureAngle: e.target.value })
-                  }
-                />
-              </div>
-              {gear === "Spur Gear" ? (
-                <div>
-                  <label>Sut* :</label>
-                  <Input
-                    aria-label="Sut"
-                    type="text"
-                    required
-                    className="mt-2 focus:border-blue-600"
-                    onChange={(e) => setData({ ...data, sut: e.target.value })}
-                  />
-                </div>
-              ) : (
-                ""
-              )}
+            ) : (
+              ""
+            )}
 
-              {gear === "Helical Gear" ? (
-                <div>
-                  <label>Helix Angle* :</label>
-                  <Input
-                    aria-label="helix angle"
-                    type="text"
-                    required
-                    className="mt-2 focus:border-blue-600"
-                    onChange={(e) =>
-                      setData({ ...data, helixAngle: e.target.value })
-                    }
-                  />
-                </div>
-              ) : (
-                ""
-              )}
-
-              <div className="pt-1">
-                <Button className="w-full text-white ring-offset-2 ring-blue-600 focus:ring bg-blue-600 hover:bg-blue-500 active:bg-gray-900">
-                  Calculate
-                </Button>
+            {gear === "Helical Gear" ? (
+              <div>
+                <label>Helix Angle* :</label>
+                <Input
+                  aria-label="helix angle"
+                  type="text"
+                  required
+                  className="mt-2 focus:border-blue-600"
+                  onChange={(e) =>
+                    setData({ ...data, helixAngle: e.target.value })
+                  }
+                />
               </div>
-            </form>
-          </div>
-        </>
-      )}
+            ) : (
+              ""
+            )}
+
+            <div className="pt-1">
+              <Button className="w-full text-white ring-offset-2 ring-blue-600 focus:ring bg-blue-600 hover:bg-blue-500 active:bg-gray-900">
+                Calculate
+              </Button>
+            </div>
+          </form>
+        </div>
+      </>
     </>
   );
 }
--- pages/result/index.jsx
+++ pages/result/index.jsx
@@ -1,30 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/ui/Navbar";
 import { useRouter } from "next/router";
 import gear from "../../public/Spur_gears_animation.gif";
+import animation from "../../public/animation.json";
 
 import { tableData } from "./data";
 import Image from "next/image";
+import Lottie from "lottie-react";
 
 const Index = () => {
+  const [showLoading, setShowLoading] = useState(true);
+
   const router = useRouter();
   const data = router.query;
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowLoading(false);
+    }, 3000);
+  }, []);
 
   return (
     <div>
       <Navbar />
-      {tableData[data.sut] ? (
-        <div className="px-64 pt-10">
-          <h4 className="heading-h2 pb-2 text-3xl font-semibold tracking-tight first:mt-0 mt-10">
-            Sut : {tableData[data.sut]}
-          </h4>
-        </div>
+      {showLoading ? (
+        <Lottie
+          animationData={animation}
+          loop
+          className="pt-8 pb-12 pl-96 pr-96"
+        />
       ) : (
-        ""
+        <>
+          {tableData[data.sut] ? (
+            <div className="px-64 pt-10">
+              <h4 className="heading-h2 pb-2 text-3xl font-semibold tracking-tight first:mt-0 mt-10">
+                Sut : {tableData[data.sut]}
+              </h4>
+            </div>
+          ) : (
+            ""
+          )}
+          <div className="flex justify-center py-32">
+            <Image src={gear} alt="gear" width={500} />
+          </div>
+        </>
       )}
-      <div className="flex justify-center py-32">
-        <Image src={gear} alt="gear" width={500} />
-      </div>
     </div>
   );
 };
