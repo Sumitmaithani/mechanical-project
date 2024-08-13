9c87326702c7898257c3384da93b31dab6c6ed0a
Sumitmaithani <sumitmaithani824@gmail.com>
2024-08-13 18:52:06 +0530

    fix: build issue

--- components/ui/Brand/index.jsx
+++ components/ui/Brand/index.jsx
@@ -1,13 +1,4 @@
 import Link from "next/link";
 
-const Brand = () => (
-    <Link href="/">
-        <img
-            src="/blinder.svg"
-            width={120}
-            height={50}
-            alt="Blinder logo"
-        />
-    </Link>
-)
-export default Brand
+const Brand = () => <Link href="/"></Link>;
+export default Brand;
